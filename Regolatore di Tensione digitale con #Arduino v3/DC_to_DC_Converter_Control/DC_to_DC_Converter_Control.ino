4c6c3d3703db010455a80340140ba7d162218249
MastrMarco <marcomastrangelo33@gmail.com>
2023-04-18 18:21:34 +0200

    Regolatore di Tensione digitale con #Arduino v3

---
+++ Regolatore di Tensione digitale con #Arduino v3/DC_to_DC_Converter_Control/DC_to_DC_Converter_Control.ino
@@ -0,0 +1,125 @@
+//*****************************************************************************************************************************//
+//-------------------          By  MastrMarco    Versione 1.0        ISCRIVITI AL CANALE YOUTUBE       ------------------------//
+//
+//                                  https://www.youtube.com/channel/UCpQb1Iz6M229ylkyXpdJPlw
+//*****************************************************************************************************************************//
+/*
+*Code By MastrMarco
+*Libreria by: https://github.com/GitMoDu/FastX9CXXX
+*/
+
+#include <FastX9CXXX.h>
+#define X9_CS_PIN 10
+#define X9_UD_PIN 8
+#define X9_INC_PIN 9
+FastX9C103 Potentiometer(X9_CS_PIN, X9_UD_PIN, X9_INC_PIN);
+
+
+// Lettura Tensione di Uscita
+#define Analog_Volt_OUT A0
+int datiS;
+int Campionamenti = 5;  // Numero di Campionamenti
+int CampNum;
+float Con;
+float R1 = 30000;  // => 30k
+float R2 = 7500;   // => 7.5K
+float Volt;
+//Delay Virtuale
+unsigned long TempoVal = 0;
+int DelayVirtuale2 = 50;
+
+
+//Valori impostabili
+int Val_Resistore = 50;  // 0 - 100
+float Volt_Set = 0;  // 0 - 100
+
+
+//Delay Virutale
+unsigned long Tempo0 = 0;
+int DelayVirtuale = 1000;
+
+
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(Analog_Volt_OUT, INPUT);
+
+  Potentiometer.Reset();
+
+  Volt_Set = 5;
+}
+
+
+//----------------------------------
+float Tensione() {
+  if (millis() >= (TempoVal + DelayVirtuale2)) {
+    // leggere il valore sull'ingresso analogico
+    if (CampNum < Campionamenti) {
+      datiS += analogRead(Analog_Volt_OUT);
+      CampNum++;
+    } else {
+      Con = ((datiS / Campionamenti) * 5.0) / 1024;
+      Volt = Con / (R2 / (R1 + R2));
+      datiS = 0;
+      CampNum = 0;
+      ajustaResistore();
+    }
+    TempoVal = millis();
+  }
+  return Volt;
+}
+
+
+//----------------------------------
+void ajustaResistore() {
+  if (Volt_Set > Tensione()) {
+    if (Val_Resistore > 0) {
+      Val_Resistore--;
+      Potentiometer.JumpToStep(Val_Resistore);
+    }
+  } else {
+    if (Val_Resistore < 100) {
+      Val_Resistore++;
+      Potentiometer.JumpToStep(Val_Resistore);
+    }
+  }
+}
+
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    String rx = Serial.readString();
+    Volt_Set = rx.toFloat();
+  }
+
+  if (((Volt_Set - Tensione()) >= 0.35) or ((Tensione() - Volt_Set) >= 0.35)) {
+    DelayVirtuale2 = 5;
+    Campionamenti = 1;
+  } else {
+    DelayVirtuale2 = 500;
+    Campionamenti = 5;
+  }
+  Tensione();
+
+
+
+  //Serial Monitor
+  if (millis() >= (Tempo0 + DelayVirtuale)) {
+    Tempo0 = millis();
+
+    Serial.print(Volt_Set, 2);
+    Serial.println(" Volt_SET");
+
+    Serial.print(Tensione(), 2);
+    Serial.println(" Volt");
+
+    Serial.print(Potentiometer.GetEstimatedResistance(), DEC);
+    Serial.println(F(" Ohm"));
+
+    Serial.print(Campionamenti);
+    Serial.println(" Campionamenti");
+  }
+}
---
+++ Regolatore di Tensione digitale con #Arduino v3/README.md
@@ -0,0 +1,11 @@
+# Regolatore di Tensione digitale con #Arduino v2
+
+## Schema
+
+![Schematic_Arduino DC-DC Converter_2023-04-08](https://user-images.githubusercontent.com/76437833/230718551-45a2297a-7723-4dc6-a833-5fcc91062933.png)
+
+## 
+![L293D Out](https://user-images.githubusercontent.com/76437833/230718566-e528e34e-298e-4ad0-b8e4-59329ae38f34.jpg)
+
+##
+
